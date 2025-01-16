0d08f8c2005bc2a602d39affdb996f22f04087c5
Michael Hahn <skipiano@gmail.com>
2025-01-16 13:39:20 +0900

    build towers

--- java/src/sprint/Soldier.java
+++ java/src/sprint/Soldier.java
@@ -1,7 +1,6 @@
 package sprint;
 
-import battlecode.common.GameActionException;
-import battlecode.common.RobotInfo;
+import battlecode.common.*;
 
 public class Soldier extends Unit {
 
@@ -10,12 +9,13 @@ public class Soldier extends Unit {
 
     // Currently this might be false but unit still might be within tower range, i.e. low paint
     public static boolean aggressiveMode = false;
+    public static boolean builder = false;
 
     public void act() throws GameActionException {
         super.act();
         attack();
-//        build();
-//        paintSRP();
+        build();
+        paintSRP();
         move();
     }
 
@@ -33,7 +33,106 @@ public class Soldier extends Unit {
         aggressiveMode = false;
     }
 
+    public void build() throws GameActionException {
+        MapLocation[] ruins = rc.senseNearbyRuins(-1);
+
+        if (ruins.length == 0) {
+            builder = false;
+            return;
+        }
+
+        MapLocation closestRuin = null;
+        int minDist = GameConstants.VISION_RADIUS_SQUARED;
+        for (MapLocation ruin : ruins) {
+            RobotInfo robot = rc.senseRobotAtLocation(ruin);
+            if (robot == null) {
+                int dist = rc.getLocation().distanceSquaredTo(ruin);
+                if (dist <= minDist) {
+                    minDist = dist;
+                    closestRuin = ruin;
+                }
+            }
+        }
+
+        if (closestRuin == null) {
+            return;
+        }
+
+        builder = true;
+        UnitType type = towerType(rc.getLocation(), closestRuin);
+        MapInfo[] nearbyLocations = rc.senseNearbyMapInfos(closestRuin, 8);
+
+        MapLocation bestPaintLocation = null;
+        int closestDist = UnitType.SOLDIER.actionRadiusSquared;
+
+        for (MapInfo info : nearbyLocations) {
+            if (!(info.getPaint().isAlly() && info.getPaint().isSecondary() == Util.useSecondaryForTower(info.getMapLocation(), closestRuin, type))) {
+                if (rc.getLocation().distanceSquaredTo(info.getMapLocation()) <= closestDist) {
+                    closestDist = rc.getLocation().distanceSquaredTo(info.getMapLocation());
+                    bestPaintLocation = info.getMapLocation();
+                }
+            }
+        }
+
+        if (bestPaintLocation != null) {
+            if (rc.canPaint(bestPaintLocation)) {
+                rc.attack(bestPaintLocation, Util.useSecondaryForTower(bestPaintLocation, closestRuin, type));
+            } else {
+                if (rc.getPaint() < UnitType.SOLDIER.attackCost) {
+                    builder = false;
+                }
+            }
+        }
+        if (rc.getLocation().distanceSquaredTo(closestRuin) <= GameConstants.BUILD_TOWER_RADIUS_SQUARED) {
+            if (rc.canCompleteTowerPattern(type, closestRuin)) {
+                rc.completeTowerPattern(type, closestRuin);
+                builder = false;
+            }
+        } else {
+            Navigator.moveTo(closestRuin);
+        }
+    }
+
+    public void paintSRP() throws GameActionException {
+        
+    }
+
     public void move() throws GameActionException {
 
+    }
+
+    public static UnitType towerType(MapLocation loc, MapLocation ruin) {
+        double rand = randomFunction(loc.x, loc.y);
+        if (rc.getRoundNum() < 200) {
+            if (rand < 0.3) {
+                return UnitType.LEVEL_ONE_PAINT_TOWER;
+            } else {
+                return UnitType.LEVEL_ONE_MONEY_TOWER;
+            }
+        } else if (rc.getRoundNum() < 1000) {
+            if (rand < 0.5) {
+                return UnitType.LEVEL_ONE_PAINT_TOWER;
+            } else if (rand < 1) {
+                return UnitType.LEVEL_ONE_MONEY_TOWER;
+            } else {
+                return UnitType.LEVEL_ONE_DEFENSE_TOWER;
+            }
+        } else {
+            if (rand < 0.5) {
+                return UnitType.LEVEL_ONE_PAINT_TOWER;
+            } else if (rand < 1) {
+                return UnitType.LEVEL_ONE_MONEY_TOWER;
+            } else {
+                return UnitType.LEVEL_ONE_DEFENSE_TOWER;
+            }
+        }
+    }
+
+    public static double randomFunction(int x, int y) {
+        int hash = (x * 0x1f1f1f1f) ^ (y * 0x2e2e2e2e);
+        hash = ((hash >>> 16) ^ hash) * 0x45d9f3b;
+        hash = ((hash >>> 16) ^ hash) * 0x45d9f3b;
+        hash = (hash >>> 16) ^ hash;
+        return (hash) / (double) 0xFFFFFFFF;
     }
 }
